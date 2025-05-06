d2d87fde5e03844fb30cc74f2ff2207416f8818b
Abraão Eber Gomes Batista <abraaoeber@academicos.com>
2025-05-06 20:36:26 -0300

    Abraao: Modifica script__brenner.py com novas modificações

---
+++ script_abraao.py
@@ -0,0 +1,10 @@
+def saudacao_brenner():
+    print("olá do Brenner")
+
+def saudacao_abraao():
+    print("oi do abraao, coloborando com Brenner lindo")
+
+if ___name__ == "__main__":
+    saudacao_brenner()
+    saudacao_abraao() 
+        print("Arquivo modificado por Abraao")
